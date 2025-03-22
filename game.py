f827b6c802f8e94a9011cc90f647a298e9e8f257
Lucaszabalare <nellymarac@gmail.com>
2025-03-22 11:25:10 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -12,14 +12,83 @@ BLACK = "black"
 gallaga = Actor("gallaga")
 gallaga.pos = (WIDTH / 2,HEIGHT - 60)
 
+is_game_over = False
 bullets = []
-
+score = 0
+lives = 3
 enemies = []
 for i in range(8):
-    bee = Actor("bee")
-    bee.x = random.randint(0,WIDTH - 80)
-    bee.y = random.randint(-100,0)
-    enemies.append(bee)
+    enemy = Actor("bee")
+    enemy.x = random.randint(0,WIDTH - 80)
+    enemy.y = random.randint(-100,0)
+    enemies.append(enemy)
+
+speed = 5
+# Function to display score
+def display_score():
+    screen.draw.text(f"Score: {score}",(50,30))
+    screen.draw.text(f"Lives: {lives}",(50,60))
+
+# Function to handle bullets
+def on_key_down(key):
+    if key == keys.SPACE:
+        bullet = Actor("bullet")
+        bullet.x = gallaga.x
+        bullet.y = gallaga.y - 50
+        bullets.append(bullet)
+
+def update():
+    global score
+    global lives
+    
+    # Moving the ship right or left
+    if keyboard.left:
+        gallaga.x -= speed
+        if gallaga.x <= 0:
+            gallaga.x = 0
+    if keyboard.right:
+        gallaga.x += speed
+        if gallaga.x >= WIDTH:
+            gallaga.x = WIDTH
+    # Move bullets
+    for bullet in bullets:
+        if bullet.y <= 0:
+            bullets.remove(bullet)
+        else:
+            bullet.y -= 10
+    
+    # Move enemies
+    move_down = False
+    for enemy in enemies:
+        enemy.y += 5
+        if enemy.y > HEIGHT:
+            enemy.x = random.randint(0,WIDTH - 80)
+            enemy.y = random.randint(-100,0)
+        # Check collision with bullets
+        for bullet in bullets:
+            if enemy.colliderect(bullet):
+                sounds.eep.play()
+                score += 100
+                bullets.remove(bullet)
+                enemies.remove(enemy)
+        # Check collision with gallaga
+        if enemy.colliderect(gallaga):
+            lives -= 1
+            enemies.remove(enemy)
+            if lives == 0:
+                game_over()
+
+    # Continue creating enemies
+    if len(enemies) < 8:
+        enemy = Actor("bee")
+        enemy.x = random.randint(0,WIDTH - 80)
+        enemy.y = random.randint(-100,0)
+        enemies.append(enemy)
+
+
+
+
+
 
 
 pgzrun.go()
