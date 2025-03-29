b2fc225c47dbbeeeb9d96482dcc8e45363f9f3e9
Lucaszabalare <nellymarac@gmail.com>
2025-03-29 11:21:29 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -85,10 +85,41 @@ def update():
         enemy.y = random.randint(-100,0)
         enemies.append(enemy)
 
+# Function to draw game state
+def draw():
+    if lives > 0:
+        screen.clear()
+        screen.fill("#04052E")
+        gallaga.draw()
+        for enemy in enemies:
+            enemy.draw()
+        for bullet in bullets:
+            bullet.draw()
+        display_score()
+    else:
+        game_over()
 
+def game_over():
+    is_game_over = True
+    screen.clear()
+    screen.fill("#EDF5FC")
+    screen.draw.text("Game Over!",(WIDTH // 2 - 180,HEIGHT // 2 - 40),fontsize = 60,color = "Black")
+    screen.draw.text(f"Final Score:{score}",(WIDTH // 2 - 180,HEIGHT // 2 + 15),fontsize = 45,color = "Black")
+    screen.draw.text("Press SPACE to play again!",(WIDTH // 2 - 180,HEIGHT // 2 + 60),fontsize = 45,color = "Black")
+    
+    if keyboard.SPACE:
+        restart_game()
 
-
-
-
+def restart_game():
+    global bullets, lives, score, enemies
+    score = 0
+    lives = 3
+    bullets = []
+    enemies = []
+    for i in range(8):
+        enemy = Actor("bee")
+        enemy.x = random.randint(0,WIDTH - 80)
+        enemy.y = random.randint(-100,0)
+        enemies.append(enemy)
 
 pgzrun.go()
